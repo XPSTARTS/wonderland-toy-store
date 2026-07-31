fde8d6682071beb54ed2a36361d4b8c5770afb82
XPSTARTS <abdulmoid47628@gmail.com>
2026-07-31 14:23:02 +0500

    Remove duplicate folders causing build failures

--- Services/IRedisCacheService.cs
+++
@@ -1,7 +0,0 @@
-﻿public interface IRedisCacheService
-{
-    Task SetAsync<T>(string key, T value, TimeSpan? expiry = null);
-    Task<T?> GetAsync<T>(string key);
-    Task RemoveAsync(string key);
-    Task<bool> ExistsAsync(string key);
-}
