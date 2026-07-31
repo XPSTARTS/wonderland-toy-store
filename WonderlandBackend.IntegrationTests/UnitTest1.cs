39d3f0adc40d023d440c513a50b1848a24e0517a
XPSTARTS <abdulmoid47628@gmail.com>
2026-07-31 14:15:46 +0500

    Setup .NET 8 backend with xUnit testing and CI pipeline

---
+++ WonderlandBackend.IntegrationTests/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace WonderlandBackend.IntegrationTests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
---
+++ WonderlandBackend.UnitTests/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace WonderlandBackend.UnitTests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
